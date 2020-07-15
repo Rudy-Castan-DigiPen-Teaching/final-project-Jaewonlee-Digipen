86377b723fea5ffa425dfe24dbbc8f15fd66d057
JaewonLee-Digipen <65111797+Jaewonlee-Digipen@users.noreply.github.com>
2020-07-15 21:30:44 +0900

    Add files via upload

--- SceneCommon.js
+++ SceneCommon.js
@@ -1,11 +1,11 @@
+//Jaewon Lee, Final Project, cs099, Spring, 2020
 
-
-function DrawTitle(label){
-    push();
-    textAlign(CENTER);
-    textSize(50);
-    fill(255);
-    noStroke();
-    text(label, width/2, 50);
-    pop();
+function DrawTitle(label) {
+  push();
+  textAlign(CENTER);
+  textSize(50);
+  fill(255);
+  noStroke();
+  text(label, width / 2, 50);
+  pop();
 }
